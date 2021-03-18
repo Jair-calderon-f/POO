617400b312ca24c0f5d3e975bfeebd17f10e4737
jair calderon <jaircalderon1608@gmail.com>
2021-03-18 14:19:37 -0600

    intansear

---
+++ MyMedical/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Doctor MyDoctor= new Doctor();
+        MyDoctor.name="jair Calderon";
+        MyDoctor.showName();
+    }
+}
