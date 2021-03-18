5abd4705dea813f89b9cb2e560e4c8fd4eddc059
jair calderon <jaircalderon1608@gmail.com>
2021-03-18 16:40:41 -0600

    static

--- MyMedical/src/Doctor.java
+++ MyMedical/src/Doctor.java
@@ -1,9 +1,21 @@
 public class Doctor {
-    int id;
+    static int id=0; //auutoincrement
     String name;
     String speciality;
     //comportamientos
+    Doctor(){
+        System.out.println("construyendo el objeto Doctor ");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("nombre de doctor es:"+name);
+    }
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("el id es :"+id);
     }
 }
--- MyMedical/src/Main.java
+++ MyMedical/src/Main.java
@@ -1,7 +1,20 @@
+import javax.print.Doc;
+import java.util.Scanner;
+//import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
         Doctor MyDoctor= new Doctor();
         MyDoctor.name="jair Calderon";
         MyDoctor.showName();
+        MyDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor.id++;
+
+        Doctor myDoctorAnn= new Doctor();
+        myDoctorAnn.showId();
+        System.out.println(Doctor.id);
+
+      //showMenu();
     }
 }
