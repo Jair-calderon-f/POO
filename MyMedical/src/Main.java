b87a2319c3f30d5584e3cfc1313fb36e407850a5
jair calderon <jaircalderon1608@gmail.com>
2021-03-23 11:55:03 -0600

    clases anonimas

--- MyMedical/src/Main.java
+++ MyMedical/src/Main.java
@@ -1,6 +1,4 @@
-import model.Doctor;
-import model.Patient;
-import model.User;
+import model.*;
 
 import java.util.Date;
 
@@ -22,7 +20,23 @@ public class Main {
         User userP=new Patient("lalo","lalo@gmail.com");
         userP.showDataUser();
         System.out.println("============================");
-        
+
+        User user1 = new User("pepe","pepe@gmail.com") {
+            @Override
+            public void showDataUser() {
+                System.out.println("Doctor");
+                System.out.println("Hospital: Crus verde");
+                System.out.println("Departamento:Geriatria ");
+            }
+        };
+        user1.showDataUser();
+
+        ISchedulable iSchedulable= new ISchedulable() {
+            @Override
+            public void schedule(Date date, String time) {
+
+            }
+        };
 
         /*for (model.Doctor.AvailableAppointment available:myDoctor.getAvailableAppointments()) {
             System.out.println(available.getDate()+"  "+available.getTime());
